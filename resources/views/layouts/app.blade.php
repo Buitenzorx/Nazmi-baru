dbceed72081302f63d9f1e7c0a2f79fdaca877f4
Bayu Sutra <202111224bayus@gmail.com>
2024-08-12 19:49:13 +0700

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="{{ asset('monairsu/public/img/logo-kpspams.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('Nazmi-baru/public/img/logo-kpspams.png') }}">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRkIbggVph5FhAB6AA6MOGBHRm5y0uC1knBGeox7D" crossorigin="anonymous">
     </script>
