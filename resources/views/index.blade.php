10b7eb128b968052095079d712858858ed217572
Bayu Sutra <202111224bayus@gmail.com>
2024-08-27 18:31:01 +0700

    Sumur

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -275,14 +275,8 @@
                 var wellHeight = 300; // Height of the well in pixels
                 var maxLevel = 84; // Max height level in meters
                 var waterHeight = ((84 - level) / maxLevel) * wellHeight; // Calculate the water height based on the level
-
                 $("#water").css('height', waterHeight + 'px');
-                if (waterHeight > 97){
-                    $("#water-level-value").text(97).toFixed(2));
-                }else{
-                    $("#water-level-value").text((84 - level).toFixed(2));
-                }
-                 // Update the ketinggian displayed inside the water
+                $("#water-level-value").text((84 - level).toFixed(2)); // Update the ketinggian displayed inside the water
                 // $("#distance-value").text((84 - level).toFixed(2)); // Update the jarak displayed above the water
     
                 // Determine color based on level
